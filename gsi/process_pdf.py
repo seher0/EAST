b1c4efe01bb2c436634ec3251c07cb677f4748be
nishant <nishant@github.com>
2018-04-17 12:41:22 +0530

    updates

--- gsi/process_pdf.py
+++ gsi/process_pdf.py
@@ -19,45 +19,44 @@ import numpy as np
 def path2im (src_pdf, fmt):
     images = convert_from_path(src_pdf, fmt = fmt)
     print (images[0])
-    #im = images[0].load()
-    #print im
-    arrs =  [np.transpose(np.array(im), (1, 0, 2) ) for im in images]
-    print (arrs[0].shape)
+    # im = images[0].load()
+    # print im
+    # np_arrs = [np.transpose(np.array(im), (1, 0, 2) ) for im in images]
+    np_arrs = [np.array(im) for im in images]
+    #print (np_arrs[0].shape)
 
-    return images, arrs
-
-
-def pdf2png (src_pdf, out_dir, save_fmt = None):
-    fname = src_pdf.parts[-1] 
-    src_pdf = str(src_pdf)
+    return images, np_arrs
 
 
+def pdf2png (src_pdf_path, out_dir = None, save_fmt = None):
+    src_pdf = str(src_pdf_path)
 
     pdf_im = PyPDF2.PdfFileReader(open(src_pdf, "rb"))
     npage = pdf_im.getNumPages()
 
     #if npage < 2: return
 
-    print ("--- pdf2png: ", npage)
+    print ("--- pdf2png: ", npage, src_pdf)
 
-    print (src_pdf)
-
-    images, arrs = path2im(src_pdf, fmt='ppm')
-    print (len(images))
+    images, img_np_arrs = path2im(src_pdf, fmt='ppm')
+    print ('num images created = ', len(images))
 
     if save_fmt:
+        fname = src_pdf_path.parts[-1]
         for i, im in enumerate(images):
             tail = fname + '_' + str(i) + '.' + save_fmt
-            im.save(str(out_dir / tail ), save_fmt)
+            im.save(str(out_dir / tail), save_fmt)
 
+    return img_np_arrs
 
 
 if __name__ == '__main__':
     #base = Path('/Users/nishant/UP/ocr-text-tmpl-match/data/1 - user uploaded pdfs/')
-    base = Path('../data/1 - user uploaded pdfs/')
+    base = Path('/WS/data/1 - user uploaded pdfs/')
     print (base)
     for p in list(base.glob('*.pdf')):
-        pdf2png (p, out_dir = Path('../data/1-pngs'), save_fmt = 'png')
+        #pdf2png (p, out_dir = Path('../data/1-pngs'), save_fmt = 'png')
+        pdf2png (p, out_dir = Path('../data/1-pngs'), save_fmt = None)
 
 
 
--- run_interactive.py
+++ run_interactive.py
@@ -1,17 +1,24 @@
 
-import readline, argparse
+import argparse, os
 import numpy as np
+from pathlib import Path
 from PIL import Image
 
+import logging 
+logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.INFO)
+
+import readline
 readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode vi')
 
 from run_demo_server import get_predictor, save_result, config
+from gsi.process_pdf import pdf2png
 
 
 def eval_image (predictor, img):
     '''
-    image: numpy array (W, H, C)
+    image: numpy array (H, W, C)
 
     preproc image
     build model
@@ -21,15 +28,30 @@ def eval_image (predictor, img):
     '''
     #img = np.transpose(img, (1, 0, 2)) #detects boxes but flipped outputs
     assert len(img.shape) == 3
-    print (img.shape)
+    logger.warn ('shape = ' + str(img.shape) )
+
+    shape = img.shape
+    if shape[0] < shape[1]: 
+        logger.info ('Expecting HWC format, with H > W')
+        return
 
     rst = predictor(img)
     save_result(img, rst)
+
+    dirpath = os.path.join(config.SAVE_DIR, rst['session_id'])
+    logger.warn ('Saving result to path : ' + dirpath)
 
 
 def eval_image_file (predictor, fname):
     im = Image.open(fname)
     eval_image(predictor, np.array(im))
+
+def eval_pdf_file (predictor, fname):
+    logger.warn ('\n** Evaluating pdf file : ' + fname)
+    img_np_arrs = pdf2png (fname)
+
+    for im in img_np_arrs:
+        eval_image (predictor, im)
 
 def parse_args ():
     parser = argparse.ArgumentParser()
@@ -39,17 +61,31 @@ def parse_args ():
     args = parser.parse_args()
     return args
 
-def interactive_loop (args):
-    config.SAVE_DIR = args.output
-    predictor = get_predictor(args.checkpoint_path)
+def interactive_loop (predictor):
     while True:
         line = input('Enter Image path ("q" to quit): ')
+        line = line.strip()
         if line == 'q':
             break
-        print ('File: "%s"' % line)
-        eval_image_file(predictor, line.strip())
+        logger.info ('File: "%s"' % line)
+
+        path = Path(line)
+        if path.suffix == '.png':
+            eval_image_file(predictor, str(path))
+        elif path.suffix == '.pdf':
+            eval_pdf_file (predictor, str(path))
+
+def try_all_user_pdfs (predictor):
+    base = Path('/WS/data/1 - user uploaded pdfs/')
+    #print (base)
+    for p in list(base.glob('*.pdf')):
+        eval_pdf_file (predictor, str(p))
 
 
 if __name__ == '__main__':
     args = parse_args() 
-    interactive_loop (args)
+    config.SAVE_DIR = args.output
+    predictor = get_predictor(args.checkpoint_path)
+
+    #interactive_loop (args)
+    try_all_user_pdfs (predictor)
